38ebe058727bb02510986bb1fe98cba50477dc03
gaurav ashok ghuge <130838374+gauravghuge7@users.noreply.github.com>
2024-01-28 00:30:46 +0530

    add first file for assignment

---
+++ index.assignment.js
@@ -0,0 +1,178 @@
+// assignment of javascript modules
+
+  // example for string data types 
+
+  let name = "gaurav ashok ghuge";
+  let nameOfBrother = "vaibhav ghuge ";
+  
+  // print the string data types 
+
+  console.log(name);
+  console.log(nameOfBrother);
+
+  // example of number data type 
+
+  let a = 10;
+  let b = 20; 
+
+  sum = a + b ;
+  multiplication = a * b;
+ console.log(sum);
+ console.log(multiplication);
+
+ // example of boolean data type 
+
+ let b1 = true;
+ let b2 = false;
+ let b3 = true;
+ let b4 = false;
+
+ console.log(b1);
+ console.log(b2);
+ console.log(b3);
+ console.log(b4);
+
+ // undefined data types in javascript 
+
+ let h,j,l;
+ let n,d,v;
+
+ // example for null data types 
+
+ a = null 
+ b = null 
+
+ // object 
+
+ // object is declared as {} curly brackets 
+
+
+
+ // below is non premitive data types that are array and objects 
+
+ // example for array and printing the array 
+
+ let manoj = ["rohit", "virat", "suryakumar", "rinku", 1,2,3, true];
+
+ console.log(manoj);
+
+ let kartik = ["ishan", "ruturaj", "yashsvi", 1,2,3, true];
+ 
+ console.log(kartik);
+
+ // example for objects 
+
+ vaibhav:{
+      video: "javascript fundamentals"
+      task: "assignment task for objects "
+ }
+
+
+
+
+// 10 product list of array 
+
+
+ let eCom = [
+      "laptop",
+      2023,
+      "mobile",
+      2024,
+      "earphones",
+      2024,
+      "bluetooth mouse",
+      "glass",
+      "gaming console",
+      "lighter",
+      "keyboard set",
+      "pc"
+ ]
+
+ console.log(eCom);
+
+
+
+
+ // object that key of registration number and student name 
+
+
+ // object can't accept key in number data type then we write in the string format 
+ students: {
+      first: "gaurav"
+      second: "vaibhav"
+      third: "shubham"
+      fourth: "vishal"
+      fifth: "rinku"
+ };
+
+ console.log(students);
+
+
+  // to check the typeOf function in javascript
+
+ // string 
+
+  let namo = 'this is the string';
+  console.log(typeof namo);
+
+  //number 
+  let num1 = 12;
+  console.log(typeof num1);
+
+  let num2 = 23.5;
+  console.log(typeof num2);
+
+  let num;
+  console.log(typeof num);
+
+  let num3 = null;
+  console.log(typeof num3);
+
+  // boolean 
+
+  let boo1 = true;
+  console.log(typeof boo1);
+
+  let boo2 = false;
+  console.log(typeof boo2);
+
+  //array 
+
+  let arr1 = ["mahesh", "gitesh", "kalpesh"];
+  console.log(typeof arr1);
+
+  let arr2 = ["array1", true, 23, 3.4];
+  console.log(typeof arr2);
+
+  //objects 
+
+  garry: {
+      one: 'this is one'
+      two: 'this is two'
+      three: 'this is three'
+      four: 'this is four'
+      five: 'this is five'
+
+  }
+
+  console.log(typeof garry);
+
+  garry1: {
+      one: 'this is normal'
+      two: 'this is normal1'
+      three: 'this is normal2'
+  }
+  console.log(typeof garry1);
+
+
+
+  // valid variables 
+  let name4 = 'gaurav';
+  console.log(name4);
+  let name6 = 'ghuge';
+  console.log(name6);
+
+  //invalid variabls 
+
+  //let 4name = 'gaurav'; // type is unvalid 
+//  console.log(4name); // invalid exectetion 
